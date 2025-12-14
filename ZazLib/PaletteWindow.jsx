7acd9692269cb41c6f45506b83353b9ae57dfdc5
zaz4416 <kaguyazxc2@gmail.com>
2025-12-14 21:18:29 +0900

    Add files via upload

---
+++ ZazLib/PaletteWindow.jsx
@@ -0,0 +1,104 @@
+﻿
+// Ver.1.0 : 2025/12/14
+
+
+// アプリケーションのバージョンを取得
+function appVersion() {
+  var tmp = app.version.toString().split('.') ;
+  var res = [] ;
+  for(var i = 0, len = tmp.length ; i < len ; i++) {
+    res.push(Number(tmp[i])) ;
+  }
+  return res ;
+}
+
+
+//----------------------------------------
+//  ベースクラス CPaletteWindow
+//----------------------------------------
+  
+ // CPaletteWindowのコンストラクタをここで定義
+ function CPaletteWindow( ReSizeAble ) {
+    $.writeln( "コンストラクタ_CPaletteWindow" );
+
+    // インスタンスプロパティ (thisをつけてある変数を指す)
+    // this.m_Dialog   ← 開示しない変数とする
+
+    if ( ReSizeAble ) {
+        // リサイズ可能なダイアログを生成
+        this.m_Dialog = new Window( "palette", "", undefined, {resizeable: true} );
+    }
+    else{
+        // リサイズ固定なダイアログを生成        
+        this.m_Dialog = new Window( "palette", "", undefined, {resizeable: false} );
+    }
+
+}
+
+// CPaletteWindowのメソッドをここで定義
+//  ・ベースとしたいクラスに限り、下記のようにまとめてメソッドを記述しても良い。
+//  ・サブクラスでは、個別にメソッドを記述すること。下記のようにまとめて記述してはいけない。
+CPaletteWindow.prototype = {
+
+    InitDialog: function(DlgName) {
+        // ダイアログ追加
+        var Dlg = this.GetDlg();
+        Dlg.text          = DlgName;
+        Dlg.orientation   = "column";
+        Dlg.alignChildren = [ "fill", "fill" ];
+        Dlg.opacity       = 0.7; // （不透明度）
+        $.writeln( "ダイアログ[ "+DlgName+" ]を生成");
+    },
+
+    GetDlg: function() {
+        return ( this.m_Dialog );
+    },
+
+    ShowDlg: function() {
+        var Dlg = this.GetDlg();
+        Dlg.show();
+        $.writeln("ダイアログ表示");
+    },
+
+    CloseDlg: function() {
+        var Dlg = this.GetDlg();
+        Dlg.close();
+        $.writeln("ダイアログ閉じた");
+    },
+
+    AddEditText: function(Text) {
+        var Dlg = this.GetDlg();
+        var EdText = Dlg.add( "edittext") ;
+        EdText.text = Text;
+        return EdText;
+    },
+
+    AddStaticText: function(Text) {
+        var Dlg = this.GetDlg();
+        var StText = Dlg.add( "statictext") ;
+        StText.text = Text;
+        return StText;
+    },
+
+    AddButton: function(Text) {
+        var Dlg = this.GetDlg();
+        var Btn = Dlg.add( "button") ;
+        Btn.text = Text;
+        return Btn;
+    },
+
+    AddChkBox: function(Text, Value) {
+        var Dlg = this.GetDlg();
+        var ChkBox = Dlg.add("checkbox",undefined, "");
+        ChkBox.text = Text;
+        ChkBox.value = Value;
+        return ChkBox;
+    },
+
+    CallFunc: function( InstanceName, FuncName ) {
+        var bt = new BridgeTalk;
+        bt.target = BridgeTalk.appSpecifier;
+        bt.body = InstanceName + "." + FuncName + "();";
+        bt.send();
+    }
+} // prototype
