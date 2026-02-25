f5c977de8eaaf5e5bce7233b10546dbb638be926
zaz4416 <kaguyazxc2@gmail.com>
2026-02-25 19:25:17 +0900

    拡大鏡、ひとまず、できた

--- CViewer.jsx
+++ CViewer.jsx
@@ -120,7 +120,7 @@ function CLoupePalette() {
     
     // 200px四方の拡大表示領域
     self.m_View = self.m_Win.add("customview", [0, 0, 200, 200]);
-    self.zoom = 8; // 拡大率
+    self.zoom = 2; // 拡大率
     
     self.targetImg = null; // 表示対象のScriptUIImage
     self.centerX = 0;      // 元画像上の中心Xピクセル
@@ -131,28 +131,29 @@ function CLoupePalette() {
 
         var ViewSelf = this;
 
+        var ViewWidth  = self.m_View.size.width;
+        var ViewHeight = self.m_View.size.height;
+
         //if (!self.targetImg) return;
         var g = ViewSelf.graphics;
 
         // 背景を白で塗りつぶす処理
         var whiteBrush = g.newBrush(g.BrushType.SOLID_COLOR, [1.0, 1.0, 1.0, 1.0]); // [R, G, B, A]
-        g.rectPath(0, 0, 200, 200);
+        g.rectPath(0, 0, ViewWidth, ViewHeight);
         g.fillPath(whiteBrush);
 
-        // 拡大鏡のサイズ(200)に対して、元画像の何ピクセル分を切り出すか
-        var ZoomVal = 4; //self.zoom;
+        // 拡大鏡のサイズに対して、元画像の何ピクセル分を切り出すか
         var UIScale = self.m_UIScale;
-        var sampleSize = 200 / ZoomVal;
-        var srcX = self.centerX - (sampleSize / 2);
-        var srcY = self.centerY - (sampleSize / 2);
-        g.drawImage(self.targetImg, -srcX*ZoomVal , -srcY*ZoomVal, self.targetImg_W*ZoomVal, self.targetImg_H*ZoomVal );
+        var sampleSizeX = ViewWidth  / self.zoom ;
+        var sampleSizeY = ViewHeight / self.zoom ;
+        var srcX = self.centerX - (sampleSizeX / 2);
+        var srcY = self.centerY - (sampleSizeY / 2);
+        g.drawImage(self.targetImg, -srcX*self.zoom, -srcY*self.zoom, self.targetImg_W*self.zoom, self.targetImg_H*self.zoom );
 
         var blackPen = g.newPen(g.PenType.SOLID_COLOR, [0.0, 0.0, 0.0, 1.0], 1); 
         var myFont = ScriptUI.newFont("Arial", "BOLD", 20);
         g.drawString(self.targetImg_W + "," + self.targetImg_H, blackPen, 10, 5, myFont); 
         g.drawString("Mouse:"+self.centerX +","+self.centerY, blackPen, 10, 25, myFont); 
-        g.drawString(srcX+","+srcY, blackPen, 10, 45, myFont); 
-        g.drawString("sampleSize: " + sampleSize, blackPen, 10, 65, myFont); 
 
         // 1. 下地の黒い太線 (幅5px)
         var pBlack = g.newPen(g.PenType.SOLID_COLOR, [0, 0, 0, 1], 5);
